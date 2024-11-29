794757176096743ee3550e6a109dba097e95e739
david <davida1d2@gmail.com>
2024-11-28 21:39:16 -0300

    Add terrain operations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use noise::{Billow, Fbm, NoiseFn, Perlin, Worley};
+use noise::*;
 use noise::utils::{NoiseMapBuilder, PlaneMapBuilder, ColorGradient, ImageRenderer};
 use std::path::Path;
 
@@ -44,6 +44,45 @@ where
     }
 }
 
+pub struct Scalar{
+    value: f64,
+}
+
+impl Scalar{
+    pub fn new(value:f64) -> Self {
+        Scalar {
+            value,
+        }
+    }
+}
+
+impl NoiseFn<f64, 3> for Scalar {
+    fn get(&self, _point: [f64; 3]) -> f64 {
+        self.value
+    }
+}
+
+pub struct ReduceTo2D<'a, Noise> {
+    noise: &'a Noise,
+    fixed_z: f64,
+}
+
+impl<'a, Noise> ReduceTo2D<'a, Noise> {
+    pub fn new(noise: &'a Noise, fixed_z: f64) -> Self {
+        Self { noise, fixed_z }
+    }
+}
+
+impl<'a, Noise> NoiseFn<f64, 2> for ReduceTo2D<'a, Noise>
+where
+    Noise: NoiseFn<f64, 3>,
+{
+    fn get(&self, point: [f64; 2]) -> f64 {
+        // Extend the 2D point to 3D by adding the fixed_z coordinate
+        self.noise.get([point[0], point[1], self.fixed_z])
+    }
+}
+
 #[allow(unused_variables)]
 fn main() {
     //Generation properties
@@ -52,8 +91,8 @@ fn main() {
     let resolution = 1000;
 
     // Domain warp properties
-    let warp_strength = 3.5;
-    let warp_freq = 1.0;
+    let warp_strength: f64 = 1.5;
+    let warp_freq: f64 = 1.0;
 
     // Available filters
     let perlin_noise = Perlin::new(seed);
@@ -62,13 +101,22 @@ fn main() {
     let fbm_worley_noise = Fbm::<Worley>::new(seed);
     let billow_noise = Billow::<Perlin>::new(seed);
 
+    let abs_perlin_noise: Abs<f64, &Fbm<Perlin>, 3> = Abs::new(&fbm_perlin_noise);
+    let reversed_perlin_noise = Multiply::new(Add::new(&abs_perlin_noise, Scalar::new(-0.2)), Scalar::new(-2.0));
+
+    // Reduce reversed_perlin_noise to 2D
+    let reduced_perlin_noise = ReduceTo2D::new(&reversed_perlin_noise, 0.0);
+
     // Create the DomainWarp instance with generics
     let perlin_warp_noise = DomainWarp::new(&fbm_perlin_noise, &fbm_perlin_noise, warp_strength, warp_freq);
     let worley_warp_noise = DomainWarp::new(fbm_worley_noise, &fbm_perlin_noise, warp_strength, warp_freq);
     let billow_warp_noise = DomainWarp::new(billow_noise, &fbm_perlin_noise, warp_strength, warp_freq);
+    let reversed_perlin_warp_noise = DomainWarp::new(reduced_perlin_noise, &fbm_perlin_noise, warp_strength, warp_freq);
+
+    let terrain = Add::new(perlin_warp_noise, Scalar::new(0.2));
 
     // Build a noise map using PlaneMapBuilder with the wrapped noise function
-    let noise_map = PlaneMapBuilder::new(&fbm_perlin_noise)  // Use wrapped noise function
+    let noise_map = PlaneMapBuilder::new(terrain)  // Use wrapped noise function
         .set_x_bounds(-length/2.0, length/2.0)  // Set the x-axis bounds
         .set_y_bounds(-length/2.0, length/2.0)  // Set the y-axis bounds
         .set_size(resolution, resolution)  // Set the resolution of the noise map
@@ -82,5 +130,5 @@ fn main() {
     
 
     // Save the noise map to a file
-    image.write_to_file(Path::new("domain_warp.png"));
+    image.write_to_file(Path::new("terrain.png"));
 }
