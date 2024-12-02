eae725db85a33d9590e9913558908c34037716b5
david <davida1d2@gmail.com>
2024-12-02 12:15:50 -0300

    Saving height map

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use terrain_filter::*;
 #[allow(unused_variables)]
 fn main() {
     //Generation properties
-    let seed:u32 = 3;
+    let seed:u32 = 3978;
     let length = 5.0;
     let resolution = 1000;
 
@@ -23,13 +23,13 @@ fn main() {
         .set_size(resolution, resolution)  // Set the resolution of the noise map
         .build();
 
-    
+    // Generate and save height map
+    let height_map = ImageRenderer::new().render(&noise_map);
+    height_map.write_to_file(Path::new("height_map.png"));
 
+    // Generate and save colored map
     let image = ImageRenderer::new()
         .set_gradient(ColorGradient::new().build_terrain_gradient())
         .render(&noise_map);
-    
-
-    // Save the noise map to a file
     image.write_to_file(Path::new("terrain.png"));
 }
