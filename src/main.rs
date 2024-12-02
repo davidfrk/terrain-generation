7895467309be3eff08fd355cffd516e76aec48cf
david <davida1d2@gmail.com>
2024-12-02 12:56:25 -0300

    Add basic wgpu support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-
 mod terrain_filter;
-
+mod wgpu_app;
 
 use noise::utils::{NoiseMapBuilder, PlaneMapBuilder, ColorGradient, ImageRenderer};
 use std::path::Path;
 use terrain_filter::*;
+
+use winit::event_loop::EventLoop;
 
 #[allow(unused_variables)]
 fn main() {
@@ -32,4 +33,38 @@ fn main() {
         .set_gradient(ColorGradient::new().build_terrain_gradient())
         .render(&noise_map);
     image.write_to_file(Path::new("terrain.png"));
+
+
+    // WGPU
+    let event_loop = EventLoop::new().unwrap();
+    #[allow(unused_mut)]
+    let mut builder = winit::window::WindowBuilder::new();
+    
+    #[cfg(target_arch = "wasm32")]
+    {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowBuilderExtWebSys;
+        let canvas = web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .get_element_by_id("canvas")
+            .unwrap()
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .unwrap();
+        builder = builder.with_canvas(Some(canvas));
+    }
+    let window = builder.build(&event_loop).unwrap();
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+        pollster::block_on(wgpu_app::run(event_loop, window));
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init().expect("could not initialize logger");
+        wasm_bindgen_futures::spawn_local(run(event_loop, window));
+    }
 }
